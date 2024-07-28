302f82c982ad0be4a11a4d1e0f6c04238b8723a9
ryancrass <ryancrass@gmail.com>
2024-07-28 13:43:39 -0400

    Move model.cpp to inst/models

--- data-raw/data_sad_raw.R
+++ data-raw/data_sad_raw.R
@@ -14,17 +14,17 @@ times <- c(mrgsolve::tgrid(0,2,0.5),
 
 
 ##Create Dosing Data
-dose_data <- data.frame(id = 1:n_subj_total, time=0, amt = doses) %>%
-  dplyr::arrange(amt) %>%
+dose_data <- data.frame(id = 1:n_subj_total, time=0, amt = doses) |>
+  dplyr::arrange(amt) |>
   dplyr::mutate(id = 1,
          evid = 1,
          cmt = 1,
-         id = cumsum(id)) %>%
-  dplyr::group_by(amt) %>%
+         id = cumsum(id)) |>
+  dplyr::group_by(amt) |>
   dplyr::mutate(tmp = 1,
          tmp = cumsum(tmp),
-         food = ifelse(tmp > n_subj_per_dose, 1, 0)) %>%
-  dplyr::select(-tmp) %>%
+         food = ifelse(tmp > n_subj_per_dose, 1, 0)) |>
+  dplyr::select(-tmp) |>
   dplyr::ungroup()
 
 
@@ -43,7 +43,7 @@ withr::with_seed(
                          scrbl = round(
                            sample(rnorm(1000, 0.9, 0.2), n_subj_total, replace=F),
                            digits=2)
-  ) %>%
+  ) |>
     dplyr::mutate(
       crclbl = signif(((140-agebl)/scrbl)*(wtbl/72)*ifelse(sexf==1, 0.85,1),
                       digits = 3)
@@ -58,7 +58,7 @@ colnames(sim_input_data) <- toupper(colnames(sim_input_data))
 
 
 ##Load Model File
-mod <- mrgsolve::mread(here::here("src/model.cpp"))
+mod <- mrgsolve::mread(system.file("models", "model.cpp", package="pmxhelpr"))
 
 
 ##Run Simulation
@@ -69,8 +69,8 @@ withr::with_seed(
 
 
 ##Create Concentration Data
-conc_data <- simout %>%
-  dplyr::select(ID, NTIME = TIME, DV) %>%
+conc_data <- simout |>
+  dplyr::select(ID, NTIME = TIME, DV) |>
   dplyr::mutate(DV = round(DV, digits = 2),
          DV = ifelse(DV < lloq, NA_real_, DV),
          LDV = round(log(DV), digits = 4),
@@ -78,7 +78,7 @@ conc_data <- simout %>%
          CMT = 2,
          EVID = 0,
          BLQ = ifelse(NTIME == 0, -1, MDV),
-         LLOQ = lloq) %>%
+         LLOQ = lloq) |>
   dplyr::rename(ODV = DV)
 
 
@@ -89,19 +89,19 @@ withr::with_seed(
   data_sad <-
     dplyr::bind_rows(
       dplyr::rename(sim_input_data, NTIME = TIME),
-      conc_data) %>%
+      conc_data) |>
     dplyr::mutate(DOSE = AMT,
            TIME = dplyr::case_when(NTIME == 0 ~ 0,
                             .default = round(jitter(NTIME), digits=2)),
-           NDAY = floor(NTIME/24) + 1) %>%
-    dplyr::group_by(ID) %>%
-    tidyr::fill(DOSE, FOOD, SEXF:CRCLBL) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange(ID, TIME, NTIME, EVID) %>%
+           NDAY = floor(NTIME/24) + 1) |>
+    dplyr::group_by(ID) |>
+    tidyr::fill(DOSE, FOOD, SEXF:CRCLBL) |>
+    dplyr::ungroup() |>
+    dplyr::arrange(ID, TIME, NTIME, EVID) |>
     dplyr::mutate(LINE = 1,
            LINE = cumsum(LINE),
            USUBJID = paste0("STUDYNUM-SITENUM-", ID),
-           PART = ifelse(FOOD == 0, "Part 1-SAD", "Part 2-FE")) %>%
+           PART = ifelse(FOOD == 0, "Part 1-SAD", "Part 2-FE")) |>
     dplyr::select(LINE, ID, TIME, NTIME, NDAY, DOSE, AMT, EVID, ODV, LDV, CMT, MDV, BLQ, LLOQ,
            FOOD, SEXF, RACE, AGEBL, WTBL, SCRBL, CRCLBL,
            USUBJID, PART)

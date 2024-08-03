084880daa24c91141b360f30de9dadea0a0eeb88
ryancrass <ryancrass@gmail.com>
2024-08-03 19:56:41 -0400

    Change function naming conventions and document

--- R/plot_vpc_exactbins.R
+++ R/plot_vpc_exactbins.R
@@ -1,6 +1,6 @@
 #' Plot a visual predictive check (VPC) with exact time bins
 #'
-#' @description  `vpc_plot_exactbins()` is a wrapper function for [vpc::vpc()]
+#' @description  `plot_vpc_exactbins()` is a wrapper function for [vpc::vpc()]
 #' that returns a `ggplot2` object.
 #'
 #' @param sim Input dataset. Must contain the following variables: `"ID"`, `TIME`
@@ -14,27 +14,27 @@
 #' @param time_label Label for the time axis.
 #' @param ... Other arguments passed to [vpc::vpc()].
 #'
-#' @inheritParams mrgsim_vpc
+#' @inheritParams df_mrgsim_replicate
 #'
 #' @return A list containing calculated VPC information (when `vpcdb=TRUE`), or a ggplot2 object (default)
-#' @export vpc_plot_exactbins
+#' @export plot_vpc_exactbins
 #'
 #' @examples
-#' model <- model_load(model = "model")
-#' simout <- mrgsim_vpc(data = data_sad, model = model, replicates = 100,
+#' model <- model_mread_load(model = "model")
+#' simout <- df_mrgsim_replicate(data = data_sad, model = model, replicates = 100,
 #' output_vars = c(DV = "ODV"),
 #' num_vars = c("CMT", "EVID", "MDV", "NTIME", "LLOQ", "WTBL", "FOOD"),
 #' char_vars = c("USUBJID", "PART"),
 #' irep_name = "SIM")
 #'
-#' vpc_plot <- vpc_plot_exactbins(
+#' vpc_plot <- plot_vpc_exactbins(
 #' sim = simout,
 #' pcvpc = TRUE,
 #' pi = c(0.05, 0.95),
 #' ci = c(0.05, 0.95),
 #' log_y = TRUE)
 
-vpc_plot_exactbins <- function(sim,
+plot_vpc_exactbins <- function(sim,
                               pcvpc = FALSE,
                               time_vars = c(TIME = "TIME",
                                             NTIME = "NTIME"),
@@ -57,11 +57,11 @@ vpc_plot_exactbins <- function(sim,
   ##Observed Data
   obs <- sim |>
     dplyr::filter(!!dplyr::sym(irep_name) == 1) |>
-    df_calc_predcorr(strat_vars = strat_vars, output_vars = c(DV = "OBSDV")) |>
+    df_predcorr(strat_vars = strat_vars, output_vars = c(DV = "OBSDV")) |>
     dplyr::rename(OBSDV = DV, PCOBSDV = PCDV)
 
   ##Determine number of observations in each bin
-  bin_count <- nobs_bin(obs, bin_var = "NTIME", strat_vars = strat_vars)
+  bin_count <- df_nobsbin(obs, bin_var = "NTIME", strat_vars = strat_vars)
 
   ##Identify observed summary data
   obs_sum <- dplyr::left_join(obs, bin_count) |>
@@ -111,7 +111,7 @@ vpc_plot_exactbins <- function(sim,
 #' Count the non-missing observations in each exact bin
 #'
 #' @description
-#' `nobs_bin()` is a helper function to count the number of missing
+#' `df_nobsbin()` is a helper function to count the number of missing
 #'    and non-missing observations in exact bins.
 #'
 #' @param data Input dataset.
@@ -122,12 +122,12 @@ vpc_plot_exactbins <- function(sim,
 #' @return A data.frame containing one row per unique combination of
 #'    `bin_var` and `strat_vars` and new variables `n_obs`, a count
 #'    of non-missing observations, and `n_miss`, a count of missing observations.
-#' @export nobs_bin
+#' @export df_nobsbin
 #'
 #' @examples
-#' nobs_bin(data_sad)
+#' df_nobsbin(data_sad)
 #'
-nobs_bin <- function(data,
+df_nobsbin <- function(data,
                      bin_var = "NTIME",
                      strat_vars = "CMT"){
   bin_count <- data |>
@@ -145,25 +145,25 @@ nobs_bin <- function(data,
 #' Perform prediction-correction
 #'
 #' @description
-#' `df_calc_predcorr` is a helper function to perform prediction-correction
+#' `df_predcorr` is a helper function to perform prediction-correction
 #'    of observed or simulated depedent variables.
 #'
 #' @param data Input dataset
 #' @param bin_var Exact binning variable. Default is `"NTIME"`.
 #' @param strat_vars Stratifying variables. Default is `"CMT"`.
-#' @inheritParams mrgsim_vpc
+#' @inheritParams df_mrgsim_replicate
 #'
 #' @return A data.frame containing one row per unique combination of
 #'    `bin_var` and `strat_vars` and new variable `PCDV` containing
 #'    prediction-corrected observations.
-#' @export df_calc_predcorr
+#' @export df_predcorr
 #'
 #' @examples
-#' model <- model_load(model = "model")
-#' data <- df_add_pred(data_sad, model)
-#' simout <- df_calc_predcorr(data, output_vars = c(DV = "ODV"))
+#' model <- model_mread_load(model = "model")
+#' data <- df_addpred(data_sad, model)
+#' simout <- df_predcorr(data, output_vars = c(DV = "ODV"))
 #'
-df_calc_predcorr <- function(data,
+df_predcorr <- function(data,
                              bin_var = "NTIME",
                              strat_vars = "CMT",
                              output_vars = c(PRED = "PRED",

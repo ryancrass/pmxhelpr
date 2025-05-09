134637cfaac91a5f6736057242795458b319737c
ryancrass <ryancrass@gmail.com>
2025-05-08 20:14:05 -0400

    Add normal distribution and t-distribution options for 95% CI

--- R/plot_doseprop.R
+++ R/plot_doseprop.R
@@ -24,19 +24,30 @@ mod_loglog <- function(data, exp_var = "PPORRES", dose_var="DOSE"){
 #' Compute estimate table for log-log regression
 #'
 #' @param fit `lm` model object for the log-log regression
+#' @param method character string specifying the distrubtion to be used to derived the confidence interval.
+#'    Options are "normal" (default) and "tdist"
 #'
 #' @return `data.frame`
 #' @export df_loglog
 #'
 #' @examples
 #' #example needed
-df_loglog <- function(fit){
+df_loglog <- function(fit, method = "normal"){
+  if(!method %in% c("normal", "tdist"))
+    stop("method must be 'normal' or 'tdist'")
+
   tab <- data.frame(
     Intercept = stats::coef(fit)[[1]],
     Power = stats::coef(fit)[[2]],
     StandardError = sqrt(diag(stats::vcov(fit)))[[2]],
-    `95% LCL` = stats::coef(fit)[[2]] - stats::qt((1 + 0.95)/2, (length(fit$residuals)-1))*sqrt(diag(stats::vcov(fit)))[[2]],
-    `95% UCL` = stats::coef(fit)[[2]] + stats::qt((1 + 0.95)/2, (length(fit$residuals)-1))*sqrt(diag(stats::vcov(fit)))[[2]])
+    `95% LCL` = dplyr::case_when(method == "normal" ~ stats::coef(fit)[[2]] - 1.96*sqrt(diag(stats::vcov(fit)))[[2]],
+                                 method == "tdist" ~ stats::coef(fit)[[2]] -
+                                   stats::qt((1 + 0.95)/2, (length(fit$residuals)-1))*sqrt(diag(stats::vcov(fit)))[[2]],
+                                 .default = NA_real_),
+    `95% UCL` = dplyr::case_when(method == "normal" ~ stats::coef(fit)[[2]] + 1.96*sqrt(diag(stats::vcov(fit)))[[2]],
+                                 method == "tdist" ~ stats::coef(fit)[[2]] +
+                                   stats::qt((1 + 0.95)/2, (length(fit$residuals)-1))*sqrt(diag(stats::vcov(fit)))[[2]],
+                                 .default = NA_real_))
 
   return(tab)
 }
